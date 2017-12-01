06529ccacf2e42c21635aeeadad25bf37840e689
Damian <kontakt@dszymczuk.pl>
2017-12-01 17:54:44 +0100

    add CellHapiness component

---
+++ client/src/components/cellHapiness/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import './style.css';
+
+class CellHapiness extends Component {
+  render() {
+    return (
+      <div className="cellHapiness">
+        Hapiness
+      </div>
+    );
+  }
+}
+;
+
+CellHapiness.defaultProps = {};
+
+CellHapiness.propTypes = {};
+
+export default CellHapiness;
