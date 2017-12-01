8ea13d150f792adb80198dde71ac3a8a90b234e1
Damian <kontakt@dszymczuk.pl>
2017-12-01 23:10:31 +0100

    add test for cell hapiness

--- client/src/components/cellHapiness/index.test.js
+++ client/src/components/cellHapiness/index.test.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ComponentName from "./";
+import { Emoji } from 'emoji-mart'
+import CellHapiness from './';
 
-it('renders welcome message', () => {
-  // const wrapper = shallow(<ComponentName />);
-  // expect(wrapper.length).toEqual(1);
+it('renders CellHapiness component', () => {
+	const cellHapiness = shallow(<CellHapiness/>);
+	expect(cellHapiness.length).toEqual(1);
+	expect(cellHapiness.props().className).toEqual("cellHapiness");
+});
+
+it('renders Emoji component in CellHapiness with default props', () => {
+	const wrapper = shallow(<CellHapiness/>);
+	const emoji = wrapper.find(Emoji);
+	expect(emoji.length).toEqual(1);
+
+	expect(emoji.props().emoji).toEqual("");
+	expect(emoji.props().set).toEqual("messenger");
+	expect(emoji.props().size).toEqual(40);
+});
+
+it('renders Emoji component in CellHapiness with custom props', () => {
+	const wrapper = shallow(<CellHapiness emoji="smile" size={32} set="apple"/>);
+	const emoji = wrapper.find(Emoji);
+	expect(emoji.length).toEqual(1);
+
+	expect(emoji.props().emoji).toEqual("smile");
+	expect(emoji.props().set).toEqual("apple");
+	expect(emoji.props().size).toEqual(32);
 });
