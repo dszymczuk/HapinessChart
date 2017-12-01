2aa4809d2dc4caeda4e1a793a0670b9a4fd9428c
Damian <kontakt@dszymczuk.pl>
2017-12-01 17:54:50 +0100

    update chart page to use cells

--- client/src/containers/chartPage/index.js
+++ client/src/containers/chartPage/index.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import './style.css';
+import Cell from "../../components/cell/index";
+import CellDate from "../../components/cellDate/index";
+import CellHapiness from "../../components/cellHapiness/index";
+
+const row = Array.apply(null, {length: 10}).map((item, index) => {
+	return {
+		key: index
+	}
+});
+
+console.log(row);
+
 
 class MainPage extends Component {
+
 	render() {
 		return (
-			<div className="mainPage">
-				<p>Main Page</p>
+			<div className="chartPage">
+				<div className="days">
+				{row.map(item => {
+					return (<Cell key={item.key}>
+						<CellDate/>
+					</Cell>)
+				})}
+				</div>
+
+				<div className="emotion">
+				{row.map(item => {
+					return (<Cell key={item.key}><CellHapiness/></Cell>)
+				})}
+				</div>
+
+				<div className="emotion">
+					{row.map(item => {
+						return (<Cell key={item.key}><CellHapiness/></Cell>)
+					})}
+				</div>
 			</div>
 		);
 	}
