0dc4328d8af2ad55573a5cf13243976a2eee982f
Damian <kontakt@dszymczuk.pl>
2017-12-01 18:01:07 +0100

    add CellUser component

---
+++ client/src/components/cellUser/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import './style.css';
+
+class CellUser extends Component {
+  render() {
+    return (
+      <div className="cellUser">
+        User name
+      </div>
+    );
+  }
+}
+;
+
+CellUser.defaultProps = {};
+
+CellUser.propTypes = {};
+
+export default CellUser;
