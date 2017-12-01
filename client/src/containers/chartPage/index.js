106b42f22e1ddbbb7e0489a2453b49cfe9e69abf
Damian <kontakt@dszymczuk.pl>
2017-12-01 21:50:08 +0100

    create table on chart page

--- client/src/containers/chartPage/index.js
+++ client/src/containers/chartPage/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import './style.css';
-import Cell from "../../components/cell/index";
-import CellDate from "../../components/cellDate/index";
-import CellHapiness from "../../components/cellHapiness/index";
+import Row from "../../components/row";
+import Cell from "../../components/cell";
+import CellDate from "../../components/cellDate";
+import CellHapiness from "../../components/cellHapiness";
+import CellUser from "../../components/cellUser";
+
 
 const row = Array.apply(null, {length: 10}).map((item, index) => {
 	return {
@@ -10,7 +13,17 @@ const row = Array.apply(null, {length: 10}).map((item, index) => {
 	}
 });
 
-console.log(row);
+const users = Array.apply(null, {length: 10}).map((item, index) => {
+	return {
+		key: index
+	}
+});
+
+const hapiness = Array.apply(null, {length: 10}).map((item, index) => {
+	return {
+		key: index
+	}
+});
 
 
 class MainPage extends Component {
@@ -18,23 +31,30 @@ class MainPage extends Component {
 	render() {
 		return (
 			<div className="chartPage">
+				<div className="users">
+					<Row>
+						<Cell empty />
+						{users.map(item => {
+							return (<Cell key={item.key}>
+								<CellUser/>
+							</Cell>)
+						})}
+					</Row>
+				</div>
 				<div className="days">
-				{row.map(item => {
-					return (<Cell key={item.key}>
-						<CellDate/>
-					</Cell>)
-				})}
-				</div>
-
-				<div className="emotion">
-				{row.map(item => {
-					return (<Cell key={item.key}><CellHapiness/></Cell>)
-				})}
-				</div>
-
-				<div className="emotion">
 					{row.map(item => {
-						return (<Cell key={item.key}><CellHapiness/></Cell>)
+						return (
+							<Row key={item.key}>
+								<Cell>
+									<CellDate/>
+								</Cell>
+								{hapiness.map(item => {
+									return (<Cell key={item.key}>
+										<CellHapiness/>
+									</Cell>)
+								})}
+							</Row>
+						)
 					})}
 				</div>
 			</div>
