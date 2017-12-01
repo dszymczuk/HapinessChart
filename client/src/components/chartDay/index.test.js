6cb0dbedfad7002b2dd044d95d8ceb1d359de94f
Damian <kontakt@dszymczuk.pl>
2017-12-01 18:09:09 +0100

    add ChartDay component

---
+++ client/src/components/chartDay/index.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ComponentName from "./";
+
+it('renders welcome message', () => {
+  // const wrapper = shallow(<ComponentName />);
+  // expect(wrapper.length).toEqual(1);
+});
