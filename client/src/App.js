bfd1c6f3bb26e1d13b4fd4b5edca50982371d087
Damian <kontakt@dszymczuk.pl>
2017-12-01 17:31:54 +0100

    add reset.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import './reset.css';
 
 import store from './createStore';
 import AppWrapper from "./components/appWrapper";
