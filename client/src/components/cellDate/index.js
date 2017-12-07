498e3d0e85b7da48c8523136ae6344c8d0753a82
Damian <kontakt@dszymczuk.pl>
2017-12-07 22:05:24 +0100

    style cellDate component

--- client/src/components/cellDate/index.js
+++ client/src/components/cellDate/index.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
 import './style.css';
 
-class CellDate extends Component {
-  render() {
-    return (
-      <div className="cellDate">
-        Date
-      </div>
-    );
-  }
-}
-;
+const CellDate = ({date}) => {
+
+	const formatedDate = moment(date, 'DD-MM-YYYY');
+	const day = formatedDate.format('DD');
+	const month = formatedDate.format('MMMM');
+	const year = formatedDate.format('YYYY');
+
+	return (
+		<div className="cellDate">
+			<span className="day">{day}</span>
+			<span className="month">{month}</span>
+			<span className="year">{year}</span>
+		</div>
+	);
+};
 
 CellDate.defaultProps = {};
 
-CellDate.propTypes = {};
+CellDate.propTypes = {
+	day: PropTypes.string,
+	month: PropTypes.string,
+	year: PropTypes.string,
+};
 
 export default CellDate;
--- client/src/components/cellDate/index.test.js
+++ client/src/components/cellDate/index.test.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ComponentName from "./";
+import moment from 'moment';
+import CellDate from "./";
 
-it('renders welcome message', () => {
-  // const wrapper = shallow(<ComponentName />);
-  // expect(wrapper.length).toEqual(1);
+it('renders CellDate', () => {
+	const date = "23-11-2017";
+
+	const cellDate = shallow(<CellDate date={date}/>);
+
+	const formatedDate = moment(date, 'DD-MM-YYYY');
+	const momentDay = formatedDate.format('DD');
+	const momentMonth = formatedDate.format('MMMM');
+	const momentYear = formatedDate.format('YYYY');
+
+	const day = cellDate.find('.day');
+	const month = cellDate.find('.month');
+	const year = cellDate.find('.year');
+
+	expect(cellDate.length).toEqual(1);
+
+	expect(day.props().children).toEqual(momentDay);
+	expect(month.props().children).toEqual(momentMonth);
+	expect(year.props().children).toEqual(momentYear);
 });
